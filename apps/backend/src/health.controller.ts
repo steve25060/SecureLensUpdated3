40ed179694c21ad98cc7daf8ae665a948da62c27
Stavan <stavan794@gmail>
2026-07-22 20:33:45 +0530

    Update dependencies

--- apps/backend/src/health.controller.ts
+++ apps/backend/src/health.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { Public } from '../auth/decorators/public.decorator';
+import { Public } from './auth/decorators/public.decorator';
 
 @Controller()
 export class HealthController {
